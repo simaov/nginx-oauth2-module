2c449d512357a66aceb01048458eef531c5f8e0e
Aleksandr Simonchuk <simonchuk.ov@gmail.com>
2015-04-01 00:31:07 +0300

    add simple redirect, beautify

--- ngx_http_oauth2_module.c
+++ ngx_http_oauth2_module.c
@@ -6,8 +6,8 @@
 ngx_module_t ngx_http_oauth2_module;
 
 /*
-	ngx_http_impression_handler_handler is the function that runs when the request is processed. 
-	It is the function responsible for coordinating all of the actions in this module. 
+	This function runs when the request is processed. 
+	It responsibles for coordinating all of the actions in this module. 
 	The function accepts the parsed request and operates on it.
 */
 static ngx_int_t ngx_http_oauth2_handler(ngx_http_request_t *r) { 
@@ -16,8 +16,16 @@ static ngx_int_t ngx_http_oauth2_handler(ngx_http_request_t *r) {
 		// ...go to the next phase of processing
 		return NGX_DECLINED; 
 	} 
+	
 	r->main->internal = 1;
-	return NGX_DECLINED; 
+	
+	ngx_table_elt_t *h;
+    h = ngx_list_push(&r->headers_out.headers);
+    h->hash = 1;
+    ngx_str_set(&h->key, "Location");
+    ngx_str_set(&h->value, "http://google.com");
+    	
+	return NGX_HTTP_MOVED_TEMPORARILY;
 }
 
 /*
@@ -40,28 +48,29 @@ static ngx_int_t ngx_http_oauth2_init(ngx_conf_t *cf) {
 }
 
 static ngx_http_module_t ngx_http_oauth2_module_ctx = { 
-	NULL, 											/* preconfiguration */ 
+	NULL, 								/* preconfiguration */ 
 	ngx_http_oauth2_init, 				/* postconfiguration */ 
 	NULL, 								/* create main configuration */ 
-	NULL, 											/* init main configuration */ 
-	NULL, 											/* create server configuration */ 
-	NULL, 											/* merge server configuration */ 
-	NULL, 											/* create location configuration */ 
-	NULL 											/* merge location configuration */ 
+	NULL, 								/* init main configuration */ 
+	NULL, 								/* create server configuration */ 
+	NULL, 								/* merge server configuration */ 
+	NULL, 								/* create location configuration */ 
+	NULL 								/* merge location configuration */ 
 }; 
 
 
 ngx_module_t ngx_http_oauth2_module = { 
 	NGX_MODULE_V1, 
-	&ngx_http_oauth2_module_ctx, 	/* module context */ 
-	NULL, 		/* module directives */ 
-	NGX_HTTP_MODULE, 							/* module type */ 
-	NULL, 										/* init master */ 
-	NULL, 	/* init module */ 
-	NULL, 										/* init process */ 
-	NULL, 										/* init thread */ 
-	NULL, 										/* exit thread */ 
-	NULL, 										/* exit process */ 
-	NULL,   /* exit master */ 
+	&ngx_http_oauth2_module_ctx, 		/* module context */ 
+	NULL, 								/* module directives */ 
+	NGX_HTTP_MODULE, 					/* module type */ 
+	NULL, 								/* init master */ 
+	NULL, 								/* init module */ 
+	NULL, 								/* init process */ 
+	NULL, 								/* init thread */ 
+	NULL, 								/* exit thread */ 
+	NULL, 								/* exit process */ 
+	NULL,   							/* exit master */ 
 	NGX_MODULE_V1_PADDING 
 };
+
